92ecce51fe12de0e30d44e5ccc8e574fa9474bda
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-01-08 19:25:56 +0000

    Update 404.php

--- 404.php
+++ 404.php
@@ -8,26 +8,24 @@ get_header();
     <?php
 $bg = wp_get_attachment_image_url(get_field('hero_image', 'options'), 'full') ?: '/wp-content/uploads/2021/05/groundworks-hero.jpg';
 ?>
-    <!-- hero -->
-    <section id="hero" class="hero d-flex align-items-center hero--default mb-0">
-        <div class="overlay"></div>
-        <div class="hero__inner container-xl">
-            <div class="row h-100">
-                <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5"
-                    data-aos="fade">
-                    <h1 class="mb-4">404 - Page Not Found</h1>
-                    <div class="hero__content fs-5 mb-4">We can't seem to find the page you're looking for</div>
-                    <div class="hero__cta">
-                        <a class="btn btn-default--secondary mb-4" href="/">Return to Homepage</a>
+    <section class="hero mb-4" data-parallax="scroll"
+        data-image-src="<?=get_the_post_thumbnail_url(get_the_ID(), 'full')?>">
+        <div class="overlay--dark"></div>
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-8 py-5 d-lg-flex align-items-center z-index-0">
+                    <div class="text-center text-lg-start">
+                        <h1 class="mb-4">404 - Page Not Found</h1>
+                        <div class="hero__content fs-5 mb-4">We can't seem to find the page you're looking for</div>
+                        <div class="hero__cta">
+                            <a class="btn btn-default--secondary mb-4" href="/">Return to Homepage</a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-6 hero__image order-1 order-lg-2"
-                    style="background-image:url(<?=$bg?>)">
                 </div>
             </div>
         </div>
-        <div class="overlay--bottom"></div>
     </section>
 </main>
 <?php
 get_footer();
+?>
